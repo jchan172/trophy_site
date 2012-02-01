6cee6b69ccb2b2f5fb99b70e3253e99fc38554eb
Jack Chan <jack@jack-chan.com>
2012-01-31 20:39:01 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrophySite::Application.config.secret_token = '7895e1930743d594027363bb167ac8dc4e5d5dfb0c747532129bb5ece38285d411e8b22e9eb47ab081c7ec689eb05466534a1aec918428a162ad5ee83765a92c'
